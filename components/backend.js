a1e1f05d8c6fb1907422597d4d2e739f58d3f2de
Kevin Feyder <kevinfeyder@gmail.com>
2015-06-11 16:43:33 -0700

    new functions

--- components/backend.js
+++ components/backend.js
@@ -4,7 +4,6 @@ function hello(){
 
 function checkDay(){
     var day = new Date().getDate();
-    //console.log("Js day = " + day);
     return day;
 }
 
@@ -12,11 +11,55 @@ function congrats(sides){
     return Math.floor((Math.random() * sides ));
 }
 
-//--- icon controls ---//
-function iconvis(value){
-    return value
+
+
+
+//--- squares color ---//
+function whatColor(size,arr1,arr2){
+    if(size === 3){
+        return arr1;
+    }else{
+        return arr2;
+    }
+
 }
 
+//--- hint icons ---//
+
+function whichIcon(size,arr1, arr2,arr3,arr4, index){
+    if(size === 3){
+        if(index === arr1[0] || index === arr1[2] || index === arr1[6] || index === arr1[8]){
+          return "lock"
+        } else if(arr1[index] === arr2[index]){
+          return "ok"
+        } else if(arr1[index] !== arr2[index]){
+            return "close"
+        } else {return ""}
+    } else{
+        if(index === arr1[0] || index === arr1[3] || index === arr1[12] || index === arr1[15]){
+          return "lock"
+        } else if(arr1[index] === arr2[index]){
+          return "ok"
+        } else if(arr1[index] !== arr2[index]){
+            return "close"
+        } else {return ""}
+    }
+}
+
+function whichButton(size,arr1, arr2, index){
+    if(size === 3){
+        if(index === arr1[0] || index === arr1[2] || index === arr1[6] || index === arr1[8]){
+            return false
+        }else {return true}
+    }else{
+        if(index === arr2[0] || index === arr2[3] || index === arr2[12] || index === arr2[15]){
+            return false;
+        } else { return true}
+    }
+
+}
+
+/* old function remove before publish
 
 function iconType(arr1, arr2, index){
     if(index === arr1[0] || index === arr1[2] || index === arr1[6] || index === arr1[8]){
@@ -47,7 +90,7 @@ function visButton2(arr1,index){
     if(index === arr1[0] || index === arr1[3] || index === arr1[12] || index === arr1[15]){
         return false;
     } else { return true}
-}
+}*/
 
 function whichArray(size){
     if(size === 4){
